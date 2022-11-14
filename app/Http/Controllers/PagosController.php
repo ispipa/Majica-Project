3f6785391206c60f857e595fee4f2b7a87633bef
ispipa <ispipa123@gmail.com>
2022-11-14 12:26:17 +0100

    api

--- app/Http/Controllers/PagosController.php
+++ app/Http/Controllers/PagosController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Pagos;
 use Illuminate\Http\Request;
 
 class PagosController extends Controller
--- app/Http/Controllers/PisosController.php
+++ app/Http/Controllers/PisosController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Pisos;
 use Illuminate\Http\Request;
 
 class PisosController extends Controller
