15bc7ee5bfbad70ee24107fd0ddbd31ed0f69484
ispipa <ispipa123@gmail.com>
2022-11-14 12:21:30 +0100

    api

---
+++ app/Http/Controllers/PagosController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PagosController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        $aPagar = Pagos::where('usuario', $request->usuario)->get();
+        return $aPagar;
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $pago = new Pagos();
+        $pago->precio_pagos = $request->precio;
+        $pago->sala_pagos = $request->sala;
+        $pago->piso_pagos = $request->piso;
+        $pago->usuario = $request->usuario;
+        $pago->pagado = $request->pagado;
+        $pago->save();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $pago = Pagos::find($id);
+        return $pago;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+
+        $pago = Pagos::findOrFail($id);
+        $pago->precio_pagos = $request->precio;
+        $pago->pagado = $request->pagado;
+        $pago->save();
+        // return $pago;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $pago = Pagos::where("sala_pagos", $id)->delete();
+        return $pago;
+    }
+}
---
+++ app/Http/Controllers/SalasController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SalasController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $salas = Salas::all();
+        return $salas;
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $sala =  new Salas();
+        $sala->nombre_sala = $request->nombre_sala;
+        $sala->descripcion_sala = $request->descripcion_sala;
+        $sala->precio_sala = $request->precio_sala;
+        $sala->activo = $request->activo;
+        $sala->piso = $request->piso;
+        $sala->save();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $sala = Salas::find($id);
+        return $sala;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $sala = Salas::findOrfail($request->id);
+        $sala->nombre_sala = $request->nombre_sala;
+        $sala->descripcion_sala = $request->descripcion_sala;
+        $sala->precio_sala = $request->precio_sala;
+        $sala->activo = $request->activo;
+        $sala->piso = $request->piso;
+        return $sala;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $sala =  Salas::destroy($id);
+        return $sala;
+    }
+}
