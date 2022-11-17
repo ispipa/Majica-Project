fe95d019231b16e289d3c5dcc84260530f280c90
ispipa <ispipa123@gmail.com>
2022-11-17 12:40:21 +0100

    api

--- resources/js/components/Main/Main.jsx
+++ resources/js/components/Main/Main.jsx
@@ -56,6 +56,7 @@ export const Main = () => {
         let tipo_de_arte = e.target.tipo_de_arte.value;
         let descripción = e.target.descripción.value;
         //let img = e.target.img.files[0];
+        let img = "pepe";
         console.log(img)
         axios.post("http://127.0.0.1:8000/api/register", {
             name: name,
@@ -67,7 +68,7 @@ export const Main = () => {
             artist:tipo_de_artista,
             type_of_art:tipo_de_arte,
             description:descripción,
-
+            image:img
         }).then(res => {
             console.log(res)
         })
