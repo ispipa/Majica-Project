61c000d55dc905aa6c8fecc5eeca5fb779a6ccb1
Nylander26 <wolfnylander@gmail.com>
2022-11-29 19:20:21 +0100

    UPDATE mods

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="shortcut icon" type="image/png" href="{{ asset('/images/icono.png') }}">
     <link rel="shortcut icon" sizes="192x192" href="{{ asset('/images/icono.png') }}">
-    <title>Laravel</title>
+    <title>Virtual Museum</title>
 
     <!-- Fonts -->
     <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
